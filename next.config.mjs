aeff2ff98ed37b4d04018aa6cf74a31cc3e689f6
Pinfinity <125202205+pinfinity1@users.noreply.github.com>
2025-11-29 04:22:06 +0330

    fix/ image optimize

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // اگر متغیر محیطی IMAGE_UNOPTIMIZED برابر "true" بود، بهینه‌سازی را خاموش کن (برای لوکال داکر)
-    // در غیر این صورت (روی سرور)، بهینه‌سازی روشن باشد.
-    unoptimized: process.env.NEXT_PUBLIC_IMAGE_UNOPTIMIZED === "true",
-
     remotePatterns: [
       {
         protocol: "http",
@@ -21,13 +17,10 @@ const nextConfig = {
         hostname: "127.0.0.1", // محض اطمینان
         port: "9000",
       },
-      // --- قسمت مربوط به سرور واقعی ---
-      // اینجا آی‌پی یا دامنه سرور را اضافه می‌کنیم تا همیشه آماده باشد
-      // (حتی اگر الان استفاده نشود، بودنش ضرری ندارد)
       {
-        protocol: "http",
-        hostname: "YOUR_SERVER_IP_OR_DOMAIN", // <--- آی‌پی سرور را اینجا بنویسید
-        port: "9000",
+        protocol: "https",
+        hostname: "files.greenfastfood.ir",
+        port: "",
       },
     ],
   },
