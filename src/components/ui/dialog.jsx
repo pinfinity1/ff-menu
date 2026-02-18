a7889e70d7c11f379728bf7c43573f77a5343945
Pinfinity <125202205+pinfinity1@users.noreply.github.com>
2026-02-18 17:11:02 +0330

    fix: dialog close button

--- src/components/ui/dialog.jsx
+++ src/components/ui/dialog.jsx
@@ -28,7 +28,7 @@ function DialogOverlay({ className, ...props }) {
       data-slot="dialog-overlay"
       className={cn(
         "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50",
-        className
+        className,
       )}
       {...props}
     />
@@ -48,7 +48,7 @@ function DialogContent({
         data-slot="dialog-content"
         className={cn(
           "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg",
-          className
+          className,
         )}
         {...props}
       >
@@ -56,7 +56,8 @@ function DialogContent({
         {showCloseButton && (
           <DialogPrimitive.Close
             data-slot="dialog-close"
-            className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 left-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+            className="absolute top-4 left-4 z-50 rounded-full p-1 bg-white/70 backdrop-blur-md border border-white/20 shadow-sm text-gray-800 transition-all duration-200 hover:bg-white hover:text-red-600 hover:scale-105 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-brand-primary/50 [&_svg]:size-3 cursor-pointer
+  "
           >
             <XIcon />
             <span className="sr-only">Close</span>
@@ -83,7 +84,7 @@ function DialogFooter({ className, ...props }) {
       data-slot="dialog-footer"
       className={cn(
         "flex flex-col-reverse sm:flex-row sm:justify-end gap-2",
-        className
+        className,
       )}
       {...props}
     />
